826038a88c2a3112ec5f95a51293bc4b9ac182c6
Samra Sadaqat <smrsadaqat@gmail.com>
2023-04-09 15:06:00 +0500

    FEAT:ADD SCRIPT

---
+++ scripts/new.js
@@ -0,0 +1,87 @@
+// DAY 1- 6 April
+
+// alert("Thanks for your input!");
+
+
+// var thanx = "Thanks for your input!"
+//  alert(thanx);
+
+// var weight = 150;
+// weight += 25
+// alert("Weight: "+weight)
+
+//  var originalNum = 23;
+//  var numToBeAdded = 7;
+//   var newNum = originalNum + numToBeAdded;
+//   alert("Result:  " +newNum)
+
+// var caseQty = 144;
+// alert(caseQty);
+
+// var num = 10;
+// var anotherNum = 1;
+//  var popularNumber = num + anotherNum;
+//  alert(" ::"+popularNumber)
+
+// var whatsLeftOver = 9 % 3;
+// alert("lEFTOVER: "+whatsLeftOver)
+
+// var num = 1;
+// var newNum = num++;
+// alert("New Number: "+newNum)
+
+// var num = 1;
+// var newNum = ++num;
+// alert(": "+newNum)
+
+// var num = 1;
+// var newNum = --num;
+// alert("Here: "+newNum)
+
+// var totalCost = 1 + 3 * 4;
+// alert("Total: "+totalCost)
+
+// var totalCost = 1 + (3 * 4);
+// alert("Total: "+totalCost)
+
+// var totalCost = (1 + 3) * 4;
+// alert("Total: "+totalCost)
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// alert("Res: "+resultOfComputation)
+
+// resultOfComputation = ((2 * 4) * 4) + 2;
+// alert("Res: "+resultOfComputation)
+
+// resultOfComputation = (2 * 4) * (4 + 2);
+// alert("Resu: "+resultOfComputation)
+
+// var message = "Thanks for your input!";
+// alert(message);
+
+// alert("Thanks, " + userName + "!");
+
+// var message = "Thanks, ";
+// var banger = "!";
+// alert(message + banger);
+
+// var message = "Thanks, ";
+// var userName = "Susan";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+
+// alert("2 plus 2 equals " + 2 + 2);
+
+// var spec = prompt("Your species?", "human");
+// alert("Here: "+spec)
+
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+// alert("Alert: "+spec)
+
+var numberOfCats = prompt("How many cats?");
+var tooManyCats = numberOfCats + 1;
+alert("Input: "+tooManyCats)
+
